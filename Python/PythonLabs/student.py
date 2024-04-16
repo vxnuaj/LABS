6cfb4c64e2f58ad392def1cbb8d126d5f4c4a8a1
vxnuaj <jv.100420@gmail.com>
2024-04-15 22:17:18 -0400

    UPDATE

--- Python/PythonLabs/student.py
+++ Python/PythonLabs/student.py
@@ -1,28 +1,41 @@
 class Student:
     def __init__(self, name, house):
-        self.name = name
-        self.house = house
 
         if not name:
             raise ValueError("Missing name!")
+    
+        self.name = name
+        self.house = house
         
     def __str__(self):
         statement = f"{self.name} from {self.house}!"
         return statement
     
     @property
-    def house(self):
+    def name(self):
+        return self._name
+    
+    @name.setter
+    def name(self, name):
+        if not name:
+            raise ValueError("Missing name!")
+        self._name = name
+    
+    # A property allows for us to call an attribute in a given class to a desired specific parameter
+    @property
+    def house(self):  
         return self._house
     
+    # A getter allows for us to set rules when an attribute in a given class is assigned a new value.
+
     @house.setter
     def house(self, house):
-        if house not in ["Gryffindor", "Hufflepuff", "Ravneclaw", "Slytherin"]:
+        if house not in ["Gryffindor", "Hufflepuff", "Ravenclaw", "Slytherin"]:
             raise ValueError("Invalid house!")
         self._house = house
 
 def main():
     student = get_student()
-    student.house = "n4 privet drive"
     print(student)
 
 def get_student():
