bfda336fdf48802de7e26978dd43d594b75d9d02
vxnuaj <jv.100420@gmail.com>
2024-05-07 17:00:30 -0400

    update

---
+++ Programming/OOP/vehicles.py
@@ -0,0 +1,39 @@
+'''class Vehicle:
+    def __init__(self, name, max_speed, mileage):
+        self.name = name
+        self.max_speed = max_speed
+        self.mileage = mileage
+
+    def __str__(self):
+        return f"name: {self.name} \nspeed: {self.max_speed} \nmileage: {self.mileage}"
+
+class Bus(Vehicle):
+    def __init__(self, name, max_speed, mileage):
+        super().__init__(name, max_speed, mileage)
+
+
+
+bus = Bus('schoolbus', 50, 200)
+
+print(bus)
+
+
+'''
+
+class Vehicle:
+    def __init__(self, name, max_speed, mileage):
+        self.name = name
+        self.max_speed = max_speed
+        self.mileage = mileage
+
+    def seating_capacity(self, capacity):
+        return f"The seating capacity of a {self.name} is {capacity} passengers"
+    
+
+class Bus(Vehicle):
+    def seating_capacity(self, capacity = 50):
+        return super().seating_capacity(capacity)
+
+bus = Bus('bus', 20, 200)
+
+print(bus.seating_capacity())
